75065846a1cc443179be651efc69edcd7c6c0a55
Muhammad Fauzan <muhammadfauzanfadhlulbarr@gmail.com>
2020-10-19 10:52:56 +0700

    commit

---
+++ Latihan8.java
@@ -0,0 +1,13 @@
+public class Latihan8 {
+    
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+
+
+    }
+}
